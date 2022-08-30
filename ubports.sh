6a761db3674f4337a01ce2ca44dad84ead2db089
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2022-08-30 15:30:32 +0700

    Update ubports.sh

--- ubports.sh
+++ ubports.sh
@@ -6,28 +6,21 @@ OUTFD=/proc/self/fd/$1;
 # ui_print <text>
 ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
 
-## data/linux Touch Install For On7xelte
-twrp install /data/ubports/data/vendor.zip;
-twrp install /data/ubports/data/vendor-patches.zip;
-
 # Remove Old File
-rm -rf /data/vendor.img;
 rm -rf /data/ubuntu.img;
 
 # Copy And Patch New File
 mv -f /data/ubports/data/ubuntu.img /data/;
-mv -f /data/ubports/data/vendor.img /data/;
 mv -f /data/ubports/data/boot.img /data/;
 
 # Create Folder Mount
 mkdir -p /data/linux/Rootfs;
-mkdir -p /data/linux/System;
-mkdir -p /data/linux/Vendor;
 
 # Mount *.img to Folder Mount
 mount /data/ubuntu.img /data/linux/Rootfs;
-mount /data/linux/Rootfs/var/lib/lxc/android/android-rootfs.img /data/linux/System;
-mount /data/vendor.img /data/linux/Vendor;
+
+# Create udev
+cat /vendor/ueventd*.rc | grep ^/dev | sed -e 's/^\/dev\///' | awk '{printf "ACTION==\"add\", KERNEL==\"%s\", OWNER=\"%s\", GROUP=\"%s\", MODE=\"%s\"\n",$1,$3,$4,$2}' | sed -e 's/\r//' > /data/linux/Rootfs/etc/udev/rules.d/70-on7xelte.rules
 
 # Create EFS Like Samsung ( Fix Erfan Rootfs )
 mkdir -p /data/linux/System/efs;
@@ -35,7 +28,6 @@ mkdir -p /data/linux/System/efs;
 # Umount All *.img
 umount /data/linux/Rootfs;
 umount /data/linux/System;
-umount /data/linux/Vendor;
 
 # rename rootfs && halium boot flash
 dd if=/data/boot.img of=/dev/block/platform/13540000.dwmmc0/by-name/BOOT;
