3d333e057f255f7675eb26b79b6ad967d6025240
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2022-08-30 21:24:24 +0700

    Update ubports.sh

--- ubports.sh
+++ ubports.sh
@@ -14,27 +14,20 @@ mv -f /data/ubports/data/ubuntu.img /data/;
 mv -f /data/ubports/data/boot.img /data/;
 
 # Create Folder Mount
-mkdir -p /data/linux/Rootfs;
+mkdir -p /data/ubuntu;
 
 # Mount *.img to Folder Mount
-mount /data/ubuntu.img /data/linux/Rootfs;
-
-# Create udev
-cat /vendor/ueventd*.rc | grep ^/dev | sed -e 's/^\/dev\///' | awk '{printf "ACTION==\"add\", KERNEL==\"%s\", OWNER=\"%s\", GROUP=\"%s\", MODE=\"%s\"\n",$1,$3,$4,$2}' | sed -e 's/\r//' > /data/linux/Rootfs/etc/udev/rules.d/70-on7xelte.rules
-
-# Create EFS Like Samsung ( Fix Erfan Rootfs )
-mkdir -p /data/linux/System/efs;
+mount /data/ubuntu.img /data/ubuntu;
 
 # Umount All *.img
-umount /data/linux/Rootfs;
-umount /data/linux/System;
+umount /data/ubuntu;
 
 # rename rootfs && halium boot flash
-dd if=/data/boot.img of=/dev/block/platform/13540000.dwmmc0/by-name/BOOT;
+dd if=/data/boot.img of=/dev/block/by-name/BOOT;
 
 # Remove Install file
 rm -rf /data/boot.img;
 rm -rf /data/ubports;
-rm -rf /data/linux;
+rm -rf /data/ubuntu;
 
 ## Install Done ##
